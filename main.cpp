b8a4ca798c451bde8257d22bea7a415483582d47
Romashchenko Konstantin <WoodyAllenZombieJesus@yandex.ru>
2017-05-11 13:10:34 +0300

    OpenMP + MPI

--- main.cpp
+++ main.cpp
@@ -1,111 +1,234 @@
-#include <complex>
-#include <cmath>
 #include <iostream>
+#include <cmath>
+#include <fstream>
+#include <complex>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include "mpi.h"
 #include <omp.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-typedef std::complex<float> complexd;
 
 using namespace std;
+typedef std::complex<float> complexd;
+uint threads;
 
-int kth_bit(int k)
+void cubit(vector<complexd>& a, vector<complexd>& b, int loc_size, int N, int K1, int K2, complexd H[4][4]);
+
+
+int rank = 0, comm_size;
+
+int main(int argc, char* argv[])
 {
-    return 1 << (k-1);
-}
+    MPI_Init(&argc, &argv);
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_ARE_FATAL);
 
-int ex2(int n)
-{
-    int r = 1;
-    for (int i = 0; i < n; i++)
+    try
     {
-        r*=2;
-    }
-    return r;
-}
+        MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-int main(int argc, char** argv)
-{
-    int n,k1,k2,threads;
-    complexd *a,*b,u[4][4];
-    long long int N;
-    unsigned seed;
-    float arg;
+        if (argc != 5)
+            throw string("Wrong arguments");
 
-    if (argc < 4)
-    {
-        cout << "Недостаточно аргументов";
-        return -1;
-    }
-    n = atoi(argv[1]);
-    k1 = atoi(argv[2]);
-    k2 = atoi(argv[3]);
-    threads = atoi(argv[4]);
+        uint N = atoi(argv[1]);
+        uint K1 = atoi(argv[2]);
+        uint K2 = atoi(argv[3]);
+        threads = atoi(argv[4]);
 
-    for (int i = 0; i < 16; i++)
-    {
-        u[i / 4][i % 4] = 0;
-    }
+        uint vec_size = (uint) pow((float) 2, (float) N);
+        if (vec_size % comm_size != 0 || (uint) comm_size > vec_size)
+            throw string("Wrong number of processors");
 
-    u[0][0] = u[1][1] = u[2][3] = u[3][2] = 1;
+        uint loc_size = vec_size / comm_size;
 
-    N = 1 << n;
-    cout << N;
-    a = new complexd [N];
-    b = new complexd [N];
+        int seed = time(0);
+        double start_time, end_time, time_diff_comp = 0;
+        double all_times_comp[comm_size];
 
-    #pragma omp parallel num_threads(threads) private(seed,arg) shared(a)
-    {
-        seed = omp_get_thread_num()+time(NULL);
-        #pragma omp for
-        for (int i = 0; i < N; i++)
+        vector<complexd > a(loc_size), b(loc_size);
+
+        MPI_Bcast(&seed, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        srand(seed + rank);
+
+        float sum = 0.0;
+        float all_sum = 0.0;
+        for (uint i = 0; i < loc_size; ++i)
         {
-            arg = (float)rand_r(&seed);
-            a[i] = complex <float> (cos(arg),sin(arg));
+            a[i] = complexd((float) std::rand()/RAND_MAX, (float) std::rand()/RAND_MAX);
+            sum += std::abs(a[i])*std::abs(a[i]);
+        }
+
+        MPI_Allreduce(&all_sum, &sum, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+        all_sum = std::sqrt(all_sum);
+        for (uint i = 0; i < loc_size; ++i)
+        {
+            a[i] /= all_sum;
+        }
+
+        vector<complexd> all_res(vec_size*(rank==0));
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        start_time = MPI_Wtime();
+
+        complexd H[4][4];
+        for (int i = 0; i < 16; i++)
+        {
+            H[i / 4][i % 4] = 0;
+        }
+
+        H[0][0] = H[1][1] = H[2][3] = H[3][2] = 1;
+
+
+        cubit(a, b, loc_size, N, K1, K2, H);
+
+        end_time = MPI_Wtime();
+        time_diff_comp = end_time - start_time;
+
+        MPI_Gather(b.data(),loc_size,MPI_COMPLEX,all_res.data(),loc_size,MPI_COMPLEX,0,MPI_COMM_WORLD);
+
+        MPI_Gather(&time_diff_comp, 1, MPI_DOUBLE, all_times_comp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        if (rank == 0)
+        {
+            cout << comm_size << " " << threads << " " << N << " " << K1 << " "
+            << K2 << " " << *std::max_element(&all_times_comp[0], &all_times_comp[comm_size]) << endl;
         }
     }
-
-    double start=omp_get_wtime();
-
-
-    int shift1 = n - k1;
-    int shift2 = n - k2;
-    //Все биты нулевые, за исключением соответсвующего номеру первого изменяемого кубита
-    int pow2k1 = 1 << (shift1);
-    //Все биты нулевые, за исключением соответсвующего номеру второгоизменяемого кубита
-    int pow2k2 = 1 << (shift2);
-
-    #pragma omp parallel for shared(a,b)
-    for	(int i = 0; i < N; i++)
-    {
-        //Установка изменяемых битов во все возможные позиции
-        int i00 = i & ~pow2k1 & ~pow2k1;
-        int i01 = i & ~pow2k1 | pow2k1;
-        int i10 = (i | pow2k1) & ~pow2k1;
-        int i11 = i | pow2k1 | pow2k1;
-        //Получение значений изменяемых битов
-        int ik1 = (i & pow2k1) >> shift1;
-        int ik2 = (i & pow2k1) >> shift2;
-        //Индекс в матрице
-        int ik = (ik1 << 1) + ik2;
-        b[i] = u[ik][0] * a[i00] + u[ik][1] * a[i01] +
-            u[ik][2] * a[i10] + u[ik][3] * a[i11];
+    catch (const string& e) {
+        if (rank == 0)
+        cerr << e << endl;
+        MPI_Abort(MPI_COMM_WORLD, -1);
+    }
+    catch (const MPI::Exception& e) {
+        if (rank == 0)
+        //cerr << e.Get_error_code() << endl;
+        cerr << "MPI Exception thrown" << endl;
+        MPI_Abort(MPI_COMM_WORLD, -1);
     }
 
-    double finish=omp_get_wtime();
-
-    // cout << endl;
-    // for (int i = 0; i < N; i++)
-    // {
-    //     cout << b[i] << " ";
-    // }
-
-    delete []a;
-    delete []b;
-
-    FILE *f = fopen("./data.txt", "a");
-    fprintf(f,"%d %d %d %d %f\n",threads,n,k1,k2,finish-start);
-    fclose(f);
-
+    MPI_Finalize();
     return 0;
+}
+
+void cubit(vector<complexd>& a, vector<complexd>& b, int loc_size, int N, int K1, int K2, complexd H[4][4])
+{
+    int P1 = N - K1;
+    int stride1 = 1 << P1;
+    int P2 = N - K2;
+    int stride2 = 1 << P2;
+
+    if (stride1 < loc_size and stride2 < loc_size)
+    {
+        // All elements are here in a
+
+        #pragma omp parallel for num_threads(threads)
+        for (int i = 0; i < loc_size; ++i)
+        {
+            int i00 = i & ~stride1 & ~stride2;
+            int i01 = i & ~stride1 | stride2;
+            int i10 = (i | stride1) & ~stride2;
+            int i11 = i | stride1 | stride2;
+            int ik1 = (i & stride1) >> P1;
+            int ik2 = (i & stride2) >> P2;
+            int ik = (ik1 << 1) + ik2;
+            b[i] = H[ik][0] * a[i00] + H[ik][1] * a[i01] +
+            H[ik][2] * a[i10] + H[ik][3] * a[i11];
+        }
+    } else if (stride1 < loc_size)
+    {
+        int proc_stride = stride1 / loc_size;
+        vector<complexd> tmp(loc_size);
+
+        // MPI_Isend(a.data(),loc_size,MPI::COMPLEX,proc_stride^rank,0,MPI_COMM_WORLD);
+        //
+        // MPI_Recv(tmp.data(),loc_size,MPI::COMPLEX,proc_stride^rank,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+        MPI_Sendrecv(a.data(),loc_size,MPI::COMPLEX,proc_stride^rank,0,tmp.data(),
+        loc_size,MPI::COMPLEX,proc_stride^rank,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        #pragma omp parallel for num_threads(threads)
+        for (int i = 0; i < loc_size; ++i)
+        {
+            int i00 = i & ~stride1 & ~stride2;
+            int i01 = i & ~stride1;
+            int i10 = (i | stride1) & ~stride2;
+            int i11 = i | stride1;
+            int ik1 = (i & stride1) >> P1;
+            int ik2 = (i & stride2) >> P2;
+            int ik = (ik1 << 1) + ik2;
+
+            b[i] = H[ik][0] * a[i00] + H[ik][1] * tmp[i01] +
+            H[ik][2] * a[i10] + H[ik][3] * tmp[i11];
+
+        }
+    } else if (stride2 < loc_size)
+    {
+        int proc_stride = stride2 / loc_size;
+        vector<complexd> tmp(loc_size);
+
+        // MPI_Isend(a.data(),loc_size,MPI::COMPLEX,proc_stride^rank,0,MPI_COMM_WORLD);
+        //
+        // MPI_Recv(tmp.data(),loc_size,MPI::COMPLEX,proc_stride^rank,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+        MPI_Sendrecv(a.data(),loc_size,MPI::COMPLEX,proc_stride^rank,0,tmp.data(),
+        loc_size,MPI::COMPLEX,proc_stride^rank,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        #pragma omp parallel for num_threads(threads)
+        for (int i = 0; i < loc_size; ++i)
+        {
+            int i00 = i & ~stride1 & ~stride2;
+            int i01 = i & ~stride1 | stride2;
+            int i10 = i & ~stride2;
+            int i11 = i | stride2;
+            int ik1 = (i & stride1) >> P1;
+            int ik2 = (i & stride2) >> P2;
+            int ik = (ik1 << 1) + ik2;
+
+            b[i] = H[ik][0] * a[i00] + H[ik][1] * a[i01] +
+            H[ik][2] * tmp[i10] + H[ik][3] * tmp[i11];
+
+        }
+    } else
+    {
+        int proc_stride1 = stride1 / loc_size;
+        int proc_stride2 = stride2 / loc_size;
+        vector<complexd> tmp1(loc_size),tmp2(loc_size),tmp3(loc_size);
+
+        // MPI_Isend(a.data(),loc_size,MPI::COMPLEX,proc_stride1^rank,0,MPI_COMM_WORLD);
+        //
+        // MPI_Recv(tmp1.data(),loc_size,MPI::COMPLEX,proc_stride1^rank,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+        MPI_Sendrecv(a.data(),loc_size,MPI::COMPLEX,proc_stride1^rank,0,tmp1.data(),
+        loc_size,MPI::COMPLEX,proc_stride1^rank,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // MPI_Isend(a.data(),loc_size,MPI::COMPLEX,proc_stride2^rank,0,MPI_COMM_WORLD);
+        //
+        // MPI_Recv(tmp2.data(),loc_size,MPI::COMPLEX,proc_stride2^rank,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+        MPI_Sendrecv(a.data(),loc_size,MPI::COMPLEX,proc_stride2^rank,0,tmp2.data(),
+        loc_size,MPI::COMPLEX,proc_stride2^rank,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // MPI_Isend(a.data(),loc_size,MPI::COMPLEX,proc_stride1^proc_stride2^rank,0,MPI_COMM_WORLD);
+        //
+        // MPI_Recv(tmp3.data(),loc_size,MPI::COMPLEX,proc_stride1^proc_stride2^rank,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+        MPI_Sendrecv(a.data(),loc_size,MPI::COMPLEX,proc_stride1^proc_stride2^rank,0,tmp3.data(),
+        loc_size,MPI::COMPLEX,proc_stride1^proc_stride2^rank,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        #pragma omp parallel for num_threads(threads)
+        for (int i = 0; i < loc_size; ++i)
+        {
+            int i00 = i & ~stride1 & ~stride2;
+            int i01 = i & ~stride1;
+            int i10 = i & ~stride2;
+            int i11 = i;
+            int ik1 = (i & stride1) >> P1;
+            int ik2 = (i & stride2) >> P2;
+            int ik = (ik1 << 1) + ik2;
+            b[i] = H[ik][0] * a[i00] + H[ik][1] * tmp2[i01] +
+            H[ik][2] * tmp1[i10] + H[ik][3] * tmp3[i11];
+
+        }
+    }
 }
